bdcd31297a8e6163feae92273976c1ef0baa7d69
Valentin Sene <valentin.sene@epitech.eu>
2021-10-12 00:37:14 +0200

    [+] main.go: Add sqlite db handling

--- src/main.go
+++ src/main.go
@@ -12,10 +12,14 @@ import (
 
 	"net/http"
 
+	"database/sql"
+
 	"github.com/gin-gonic/gin"
+
+	_ "github.com/mattn/go-sqlite3"
 )
 
-type transaction struct {
+type Transaction struct {
 	House       string  `json:"house"`
 	Description string  `json:"description"`
 	Amount      float64 `json:"amount"`
@@ -23,19 +27,75 @@ type transaction struct {
 	Date        string  `json:"date"`
 }
 
-var transactions = []transaction{
-	{House: "Serdaigle", Description: "Défi AER 1", Amount: 100, Author: "Maxime", Date: "11/10/2021 16:46:00"},
-	{House: "Gryffondor", Description: "Défi AER 1", Amount: 25, Author: "Maxime", Date: "11/10/2021 16:47:00"},
-	{House: "Poufsouffle", Description: "Défi AER 1", Amount: 25, Author: "Maxime", Date: "11/10/2021 16:48:00"},
-	{House: "Serpentard", Description: "Défi AER 1", Amount: 25, Author: "Maxime", Date: "11/10/2021 16:49:00"},
+var db *sql.DB
+
+// Databases
+func createDb(db *sql.DB) {
+	db.Exec(`CREATE TABLE IF NOT EXISTS transactions(
+		Id INTEGER PRIMARY KEY AUTOINCREMENT,
+		House TEXT,
+		Description TEXT,
+		Amount INTEGER,
+		Author TEXT,
+		Date TEXT
+	);`)
 }
 
+func initDb(path string) *sql.DB {
+	db, err := sql.Open("sqlite3", path)
+
+	if err != nil || db == nil {
+		return nil
+	}
+	return db
+}
+
+func insertTransaction(db *sql.DB, t Transaction) {
+	stmt, err := db.Prepare(`
+		INSERT INTO transactions(
+			House,
+			Description,
+			Amount,
+			Author,
+			Date
+		) VALUES(?,?,?,?,?)
+	`)
+
+	if err != nil {
+		return
+	}
+	defer stmt.Close()
+	stmt.Exec(t.House, t.Description, t.Amount, t.Author, t.Date)
+}
+
+func selectTransactions(db *sql.DB) []Transaction {
+	rows, err := db.Query(`
+		SELECT House, Description, Amount, Author, Date FROM transactions
+	`)
+
+	if err != nil {
+		panic(err)
+	}
+	defer rows.Close()
+	var dest []Transaction
+	for rows.Next() {
+		item := Transaction{}
+		err = rows.Scan(&item.House, &item.Description, &item.Amount, &item.Author, &item.Date)
+		if err != nil {
+			return []Transaction{}
+		}
+		dest = append(dest, item)
+	}
+	return dest
+}
+
+// Routes
 func getTransactions(context *gin.Context) {
-	context.IndentedJSON(http.StatusOK, transactions)
+	context.IndentedJSON(http.StatusOK, selectTransactions(db))
 }
 
 func postTransactions(context *gin.Context) {
-	var new transaction
+	var new Transaction
 	err := context.BindJSON(&new)
 
 	if err != nil {
@@ -43,7 +103,7 @@ func postTransactions(context *gin.Context) {
 		return
 	}
 	new.Date = time.Now().Format("02/01/2006 15:04:05")
-	transactions = append(transactions, new)
+	insertTransaction(db, new)
 	context.IndentedJSON(http.StatusCreated, new)
 }
 
@@ -56,7 +116,12 @@ func serRouter(url string) *gin.Engine {
 	return router
 }
 
+// Main
 func main() {
+	db = initDb("./database.db")
+
+	defer db.Close()
+	createDb(db)
 	gin.SetMode(gin.ReleaseMode)
 	serRouter("localhost:4242")
 }
